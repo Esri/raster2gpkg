ca66db44eaf2fc39783704c21673f4620c4b4e09
lshipman <lshipman@esri.com>
2015-12-15 13:51:48 -0800

    add headers to python code.

--- raster2gpkg.py
+++ raster2gpkg.py
@@ -1,7 +1,30 @@
 ﻿#!/usr/bin/env python
-"""Simple script to add a dataset into an existing or new GeoPackage.
-Based on other python utilities but made PEP compliant.
-"""
+#------------------------------------------------------------------------------
+# Copyright 2015 Esri
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#------------------------------------------------------------------------------
+
+# ==================================================
+# raster2gpkg.py
+# --------------------------------------------------
+# author: Esri
+# company: Esri
+# ==================================================
+# description: Simple script to add a dataset into an existing or new GeoPackage.
+# Based on other python utilities but made PEP compliant.
+# ==================================================
+# ==========================
+
 # $Id$
 
 import arcpy
